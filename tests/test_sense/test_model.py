676dde4bec27cc492552105b1a0d2f62f95d5de7
Paul Brookes <paul.s.brookes@gmail.com>
2020-12-01 23:32:45 +0200

    Moving to unittest.

--- tests/test_sense/test_model.py
+++ tests/test_sense/test_model.py
@@ -1,111 +1,120 @@
 from normalorder.sense.model import Model
-from sympy import symbols, cos
+import sympy
 import numpy as np
+from unittest import TestCase
 
 
-def test_init():
+class TestModel(TestCase):
 
-    model = Model()
+    def setUp(self):
+        self.model = Model()
 
-    assert model.order == 0
-    assert isinstance(model.c_sym, tuple)
-    assert len(model.c_sym) == 0
-    assert isinstance(model.resonator_params,dict)
+    def test_init(self):
+        self.assertEqual(self.model.order, 0)
+        self.assertIsInstance(self.model.c_sym, tuple)
+        self.assertEqual(len(self.model.c_sym), 2)
+        self.assertIsInstance(self.model.resonator_params, dict)
 
+    def test_set_order(self):
+        for order in [0, 1, 2, 1, 0]:
+            self.model.set_order(order)
+            self.assertEqual(self.model.order, order)
+            self.assertIsInstance(self.model.c_sym, tuple)
+            self.assertEqual(len(self.model.c_sym), order + 2)
+            for m in range(order + 1):
+                self.assertIsNotNone(self.model.g_expr_gen(m))
 
-def test_set_order():
+    def test_c_and_g_expr_gen(self):
+        phi_sym, phi_ext_sym, nu_sym, n_sym = sympy.symbols('phi phi_ext nu n')
+        potential_param_symbols = {'phi_ext': phi_ext_sym,
+                                   'nu': nu_sym,
+                                   'n': n_sym}
+        potential_expr = -nu_sym * sympy.cos(phi_sym) - n_sym * sympy.cos((phi_ext_sym - phi_sym) / n_sym)
+        n = 3
+        phi_ext = 2 * np.pi * np.random.rand()
+        nu = np.random.rand() * 0.9 / n
+        potential_params = {'phi_ext': phi_ext,
+                            'n': n,
+                            'nu': nu}
+        order = 3
 
-    model = Model()
+        self.model.set_order(order)
+        self.model.set_potential(potential_expr, potential_param_symbols)
+        self.model.set_potential_params(potential_params)
 
-    for order in [0, 1, 2, 1, 0]:
-        model.set_order(order)
-        assert model.order == order
-        assert isinstance(model.c_sym, tuple)
-        assert len(model.c_sym) == order + 2
-        for m in range(order+1):
-            model.g_expr_gen(m)
+        for idx in range(order + 2):
+            phi = n * 2 * np.pi * np.random.rand()
+            assert isinstance(self.model.c_func(idx, phi), float)
 
+        for idx in range(order + 1):
+            phi = n * 2 * np.pi * np.random.rand()
+            assert isinstance(self.model.g_func(idx, phi), float)
 
-def test_set_resonator_params():
+    def test_set_resonator_params(self):
+        params = {'a': 1.0,
+                  'b': 'test'}
+        self.model.set_resonator_params(params)
+        self.assertEqual(self.model.resonator_params, params)
 
-    model = Model()
+    def test_set_potential_and_params(self):
+        phi_sym, x, y = sympy.symbols('phi x y')
+        potential_param_symbols = {'x': x,
+                                   'y': y}
+        potential_expr = x * y * phi_sym
+        self.model.set_potential(potential_expr, potential_param_symbols)
+        self.assertIsInstance(self.model.c_sym, tuple)
+        self.assertEqual(self.model.potential_expr, potential_expr)
+        self.assertEqual(self.model.potential_param_symbols, potential_param_symbols)
 
-    params = {'a': 1.0,
-              'b': 'test'}
-    model.set_resonator_params(params)
-    assert model.resonator_params == params
+        potential_params = {'x': np.random.rand(),
+                            'y': np.random.randn()}
+        self.model.set_potential_params(potential_params)
+        self.assertEqual(self.model.potential_params, potential_params)
 
+    def test_c_func(self):
+        phi_sym = sympy.symbols('phi')
+        test_c = np.random.randn(self.model.order+5)
+        offset = np.random.randn()
+        potential_expr = sum(test_c[m]*(phi_sym-offset)**m for m in range(self.model.order+2))
+        potential_param_symbols = {}
+        potential_params = {}
 
-def test_set_potential():
+        self.model.set_potential(potential_expr, potential_param_symbols)
+        self.model.set_potential_params(potential_params)
 
-    model = Model()
+        for m in range(self.model.order+2):
+            self.assertEqual(self.model.c_func(m, offset), test_c[m])
 
-    phi_sym, x, y = symbols('phi x y')
-    potential_param_symbols = {'x': x,
-                               'y': y}
-    potential_expr = x*y*phi_sym
-    model.set_potential(potential_expr, potential_param_symbols)
-    assert isinstance(model.c_sym, tuple)
-    assert model.potential_expr == potential_expr
-    assert model.potential_param_symbols == potential_param_symbols
+    def test_U_func(self):
+        self.fail()
 
-    potential_params = {'x': np.random.rand(),
-                        'y': np.random.randn()}
-    model.set_potential_params(potential_params)
-    assert model.potential_params == potential_params
+    def test_g_func(self):
+        self.fail()
 
+    def test_find_phi_min(self):
+        phi_sym, phi_ext_sym, nu_sym, n_sym = sympy.symbols('phi phi_ext nu n')
+        potential_param_symbols = {'phi_ext': phi_ext_sym,
+                                   'nu': nu_sym,
+                                   'n': n_sym}
+        potential_expr = -nu_sym * sympy.cos(phi_sym) - n_sym * sympy.cos((phi_ext_sym - phi_sym) / n_sym)
+        n = 3
+        phi_ext = 2 * np.pi * np.random.rand()
+        nu = np.random.rand() * 0.9 / n
+        potential_params = {'phi_ext': phi_ext,
+                            'nu': nu,
+                            'n': n}
+        self.model.set_potential(potential_expr, potential_param_symbols)
+        self.model.set_potential_params(potential_params)
 
-def test_cg_func():
+        self.model.find_phi_min()
+        self.assertAlmostEqual(self.model.c_func(1, self.model.phi_min), 0.0, places=14)
 
-    model = Model()
+        x0 = n * 2 * np.pi * np.random.rand()
+        self.model.find_phi_min(x0=x0)
+        self.assertAlmostEqual(self.model.c_func(1, self.model.phi_min), 0.0, places=14)
 
-    phi_sym, phi_ext_sym, nu_sym, n_sym = symbols('phi phi_ext nu n')
-    potential_param_symbols = {'phi_ext': phi_ext_sym,
-                               'nu': nu_sym,
-                               'n' : n_sym}
-    potential_expr = -nu_sym * cos(phi_sym) - n_sym * cos((phi_ext_sym - phi_sym) / n_sym)
-    n = 3
-    phi_ext = 2*np.pi*np.random.rand()
-    nu = np.random.rand()*0.9/n
-    potential_params = {'phi_ext': phi_ext,
-                        'n': n,
-                        'nu': nu}
-    order = 3
+    def test_generate_hamiltonian(self):
+        self.fail()
 
-    model.set_order(order)
-    model.set_potential(potential_expr, potential_param_symbols)
-    model.set_potential_params(potential_params)
-
-    for idx in range(order+2):
-        phi = n*2*np.pi*np.random.rand()
-        assert isinstance(model.c_func(idx, phi), float)
-
-    for idx in range(order+1):
-        phi = n*2*np.pi*np.random.rand()
-        assert isinstance(model.g_func(idx, phi), float)
-
-
-def test_find_phi_min():
-
-    model = Model()
-
-    phi_sym, phi_ext_sym, nu_sym, n_sym = symbols('phi phi_ext nu n')
-    potential_param_symbols = {'phi_ext': phi_ext_sym,
-                               'nu': nu_sym,
-                               'n' : n_sym}
-    potential_expr = -nu_sym * cos(phi_sym) - n_sym * cos((phi_ext_sym - phi_sym) / n_sym)
-    n = 3
-    phi_ext = 2*np.pi*np.random.rand()
-    nu = np.random.rand()*0.9/n
-    potential_params = {'phi_ext': phi_ext,
-                        'nu': nu,
-                        'n' : n}
-    model.set_potential(potential_expr, potential_param_symbols)
-    model.set_potential_params(potential_params)
-
-    model.find_phi_min()
-    assert np.isclose(model.c_func(1, model.phi_min), 0.0)
-
-    x0 = n*2*np.pi*np.random.rand()
-    model.find_phi_min(x0=x0)
-    assert np.isclose(model.c_func(1, model.phi_min), 0.0)
+    def test_generate_eom(self):
+        self.fail()
