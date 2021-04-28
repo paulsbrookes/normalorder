dd37567a10d2e0c1a1025fb131b4d923bc4724be
Paul <paul.s.brookes@gmail.com>
2021-04-28 23:57:33 +0300

    Operator now inhertis from SortedDict.

--- normalorder/operator/boson.py
+++ normalorder/operator/boson.py
@@ -1,34 +1,9 @@
 import numpy as np
 from sortedcontainers import SortedDict
 import functools
+import sympy
 from string import ascii_lowercase
-
-
-@functools.lru_cache(maxsize=512)
-def comm_func_raw(m, n):
-    if m == 0 or n == 0:
-        return np.zeros([0, 3])
-    else:
-        term_1 = np.array([[n, n - 1, m - 1]])
-        output_list = [term_1]
-        term_2 = np.copy(comm_func_raw(m - 1, n - 1))
-        term_2[:, 0] *= n
-        output_list.append(term_2)
-        term_3 = np.copy(comm_func_raw(m - 1, n))
-        term_3[:, 2] += 1
-        output_list.append(term_3)
-        output_array = np.vstack(output_list)
-        output_array.flags.writeable = False
-        return output_array
-
-
-def multiply_components_raw(k, l, m, n):
-    output_list = [np.array([[1, k + m, l + n]])]
-    commutator_terms = np.copy(comm_func_raw(l, m))
-    commutator_terms[:, 1] += k
-    commutator_terms[:, 2] += n
-    output_list.append(commutator_terms)
-    return np.vstack(output_list)
+from scipy.special import factorial
 
 
 def multiply_components(exponents_1, exponents_2):
@@ -78,56 +53,109 @@ def add_dictionaries(dict_1, dict_2):
     return dict_final
 
 
-def multiply_dictionary_by_scalar(dictionary, scalar):
-    output_dictionary = SortedDict()
-    for key, value in dictionary.items():
-        output_dictionary[key] = value * scalar
-    return output_dictionary
+def commutator_func(x, y):
+    if not isinstance(x, int) or not isinstance(y, int) or x < 0 or y < 0:
+        raise Exception('This function only accepts exponents which are positive integers or zero.')
+
+    def coeff_func(x, y, l):
+        coeff = factorial(x) * factorial(y) / (factorial(x - l) * factorial(y - l) * factorial(l))
+        return coeff
+
+    commutator_op = Operator(n_modes=1)
+    for l in range(1, min(x, y) + 1):
+        commutator_op[(y - l, x - l)] = coeff_func(x, y, l)
+
+    return commutator_op
 
 
-class Operator:
+def tensor(op_1, op_2):
+    op_final = Operator()
+    for key_1, coeff_1 in op_1.items():
+        for key_2, coeff_2 in op_2.items():
+            key_combined = key_1 + key_2
+            coeff_combined = coeff_1 * coeff_2
+            op_final[key_combined] = coeff_combined
+    return op_final
 
-    def __init__(self, spec=None, modes=None):
 
-        if isinstance(modes, str):
-            modes = [modes]
-        if modes is None:
-            n_modes = (np.array(spec).shape[1] - 1) // 2
-            modes = [ascii_lowercase[i] for i in range(n_modes)]
-        self.modes = modes
+class Operator(SortedDict):
 
-        ladder_op_names = []
-        for l in modes:
-            ladder_op_names += [l + '_dag', l]
-        self.ladder_op_names = ladder_op_names
+    def __init__(self, spec=None, n_modes=None):
 
-        columns = ['coeff'] + ladder_op_names
+        if (spec is None) == (n_modes is None):
+            raise Exception('Either spec or n_modes should be provided, but not both.')
 
-        if isinstance(spec, SortedDict):
-            self.data = spec
-        elif isinstance(spec, dict):
-            self.data = SortedDict(spec)
-        elif isinstance(spec, (np.ndarray, list, tuple)):
-            self.data = SortedDict()
-            for el in spec:
-                self.data[tuple(el[1:])] = el[0]
+        if n_modes is not None:
+            self.n_modes = n_modes
+            super().__init__()
         else:
-            raise Exception('Cannot handle spec input of type ' + type(spec) + '.')
+            if isinstance(spec, list):
+                spec = {tuple(element[1:]): element[0] for element in spec}
+            if len(spec) == 0:
+                raise Exception('Spec must not be empty.')
+            super().__init__(spec)
+
+            all_tuples_bool = all(list(map(lambda key: isinstance(key, tuple), self.keys())))
+            if not all_tuples_bool:
+                raise Exception('All keys should be tuples.')
+
+            unique_key_lengths = list(set(map(len, self.keys())))
+            if len(unique_key_lengths) >= 2:
+                raise Exception('All keys should be of identical length.')
+            if len(unique_key_lengths) == 1:
+                key_length = unique_key_lengths[0]
+                if key_length % 2 != 0:
+                    raise Exception('All keys should be of even length.')
+                n_modes = key_length // 2
+
+            self.n_modes = n_modes
 
     def __add__(self, other):
+        if not isinstance(other, (float, int, complex, Operator, sympy.core.mul.Mul, sympy.core.symbol.Symbol)):
+            raise Exception(
+                'The only permitted types are: float, int, comlex, Operator, sympy.core.mul.Mul and sympy.core.mul.symbols.Symbol.')
         if not isinstance(other, Operator):
-            other = Operator([[other] + [0, 0] * len(self.modes)], modes=self.modes)
-        combined_data = add_dictionaries(self.data, other.data)
-        output_operator = Operator(combined_data, modes=self.modes)
+            other = Operator({tuple(0 for i in range(2 * self.n_modes)): other})
+        output_operator = Operator({x: self.get(x, 0) + other.get(x, 0) for x in set(self).union(other)})
         return output_operator
 
     def __mul__(self, other):
+        if not isinstance(other, Operator):
+            return self.__scalar_mul__(other)
+        if self.n_modes != other.n_modes:
+            raise Exception('Both operators must have the same number of modes.')
+        if self.n_modes == 1:
+            return self.__single_mode_mul__(other)
+
         if isinstance(other, Operator):
-            output_dictionary = multiply_dictionaries(self.data, other.data)
+            output_dictionary = multiply_dictionaries(self, other)
         else:
-            output_dictionary = multiply_dictionary_by_scalar(self.data, other)
-        output_operator = Operator(spec=output_dictionary, modes=self.modes)
+            output_dictionary = multiply_dictionary_by_scalar(self, other)
+        output_operator = Operator(spec=output_dictionary)
         return output_operator
+
+    def __scalar_mul__(self, scalar):
+        output_op = Operator(n_modes=self.n_modes)
+        for exponents, coeff in self.items():
+            output_op[exponents] = coeff * scalar
+        return output_op
+
+    def __single_mode_mul__(self, other):
+        op_final = Operator(n_modes=1)
+        for exponents_1, coeff_1 in self.items():
+            for exponents_2, coeff_2 in other.items():
+                op_commutator = commutator_func(exponents_1[1], exponents_2[0])
+                product_exponents = map(lambda key: (key[0] + exponents_1[0], key[1] + exponents_2[1]),
+                                        op_commutator.keys())
+                commutator_coeffs = op_commutator.values()
+                op_product = Operator(n_modes=1)
+                for key, value in zip(product_exponents, commutator_coeffs):
+                    op_product[tuple(key)] = value
+                op_product[(exponents_1[0] + exponents_2[0], exponents_1[1] + exponents_2[1])] = 1.0
+                coeff_final = coeff_1 * coeff_2
+                op_product *= coeff_final
+                op_final += op_product
+        return op_final
 
     def __rmul__(self, other):
         return self.__mul__(other)
@@ -151,13 +179,47 @@ class Operator:
         return output_operator
 
     def copy(self):
-        return Operator(spec=self.data, modes=self.modes)
+        return Operator(spec=self)
 
     def dag(self):
         conjugated_dictionary = SortedDict()
-        for exponents, coeff in self.data.items():
+        for exponents, coeff in self.items():
             conjugated_exponents = ()
             for mode_idx in range(len(exponents) // 2):
                 conjugated_exponents += (exponents[2 * mode_idx + 1], exponents[2 * mode_idx])
             conjugated_dictionary[conjugated_exponents] = np.conjugate(coeff)
-        return Operator(spec=conjugated_dictionary, modes=self.modes)
+        return Operator(spec=conjugated_dictionary)
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, tuple):
+            raise Exception('The key must be a tuple.')
+        if len(key) != 2 * self.n_modes:
+            raise Exception('The key length must be equal to twice the number of modes.')
+        super().__setitem__(key, value)
+
+
+@functools.lru_cache(maxsize=512)
+def comm_func_raw(m, n):
+    if m == 0 or n == 0:
+        return np.zeros([0, 3])
+    else:
+        term_1 = np.array([[n, n - 1, m - 1]])
+        output_list = [term_1]
+        term_2 = np.copy(comm_func_raw(m - 1, n - 1))
+        term_2[:, 0] *= n
+        output_list.append(term_2)
+        term_3 = np.copy(comm_func_raw(m - 1, n))
+        term_3[:, 2] += 1
+        output_list.append(term_3)
+        output_array = np.vstack(output_list)
+        output_array.flags.writeable = False
+        return output_array
+
+
+def multiply_components_raw(k, l, m, n):
+    output_list = [np.array([[1, k + m, l + n]])]
+    commutator_terms = np.copy(comm_func_raw(l, m))
+    commutator_terms[:, 1] += k
+    commutator_terms[:, 2] += n
+    output_list.append(commutator_terms)
+    return np.vstack(output_list)
--- tests/test_operator/test_boson.py
+++ tests/test_operator/test_boson.py
@@ -9,49 +9,49 @@ class TestOperatorMethods(TestCase):
 
         output = a*a
         target = Operator([[1.0, 0, 2]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a*a.dag()
         target = Operator([[1.0, 1, 1], [1.0, 0, 0]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a.dag()*a
         target = Operator([[1.0, 1, 1]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a.dag()*a.dag()
         target = Operator([[1.0, 2, 0]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a*a*a
         target = Operator([[1.0, 0, 3]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a*a*a.dag()
         target = Operator([[1.0, 1, 2], [2.0, 0, 1]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a*a.dag()*a
         target = Operator([[1.0, 1, 2], [1.0, 0, 1]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a*a.dag()*a.dag()
         target = Operator([[1.0, 2, 1], [2.0, 1, 0]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a.dag()*a*a
         target = Operator([[1.0, 1, 2]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a.dag()*a*a.dag()
         target = Operator([[1.0, 2, 1], [1.0, 1, 0]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a.dag()*a.dag()*a
         target = Operator([[1.0, 2, 1]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
         output = a.dag()*a.dag()*a.dag()
         target = Operator([[1.0, 3, 0]])
-        self.assertEqual(output.data, target.data)
+        self.assertEqual(output, target)
 
