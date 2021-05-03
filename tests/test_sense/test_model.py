a07a2300d68576cf8e912a1186f14e3274b1c7d5
Paul <paul.s.brookes@gmail.com>
2021-05-03 23:05:47 +0300

    Create test to check that the frequency shift obtained perturbatively from Model using dc_func(2) agrees with results produced using Mode.

--- tests/test_sense/test_model.py
+++ tests/test_sense/test_model.py
@@ -49,6 +49,44 @@ class TestModel(TestCase):
                 delta = 1e-5
                 self.assertAlmostEqual(1.0, self.model.c_func(2)/c_2_target, delta=delta)
 
+    def test_crosscheck_frequency_shift_from_dc_2_with_Mode(self):
+
+        delta_sym, L_J_sym = sympy.symbols('delta L_J')
+        potential_param_symbols = {'L_J': L_J_sym}
+        potential_expr = (delta_sym * self.Phi_0) ** 2 / (2 * L_J_sym * constants.h)
+        delta_min_guess = 0.0
+        order = 2
+
+        Z = 50  # ohms
+        v_p = 1.1817e8  # m/s
+        l = 0.007809801198766881  # m
+        L_0 = Z / v_p  # H/m
+        C_0 = 1 / (v_p * Z)  # F/m
+        x_J = 0.002626535356654626  # m
+        L_J_1 = 1e-11
+        Delta_L_J = 0.0005 * L_J_1
+        L_J_2 = L_J_1 + Delta_L_J
+
+        model_1 = Model()
+        model_1.set_order(order)
+        model_1.set_potential(potential_expr, potential_param_symbols)
+        model_1.set_resonator_params(x_J=x_J, L_0=L_0, l=l, C_0=C_0)
+        potential_params = {'L_J': L_J_1}
+        model_1.set_potential_params(potential_params, delta_min_guess=delta_min_guess)
+        model_1.set_modes(['a'])
+
+        model_2 = Model()
+        model_2.set_order(order)
+        model_2.set_potential(potential_expr, potential_param_symbols)
+        model_2.set_resonator_params(x_J=x_J, L_0=L_0, l=l, C_0=C_0)
+        potential_params = {'L_J': L_J_2}
+        model_2.set_potential_params(potential_params, delta_min_guess=delta_min_guess)
+        model_2.set_modes(['a'])
+
+        frequency_shift_from_Mode = model_2.modes['a'].frequency - model_1.modes['a'].frequency
+        frequency_shift_from_dc_2 = (model_1.dc_func(2, 'L_J') * Delta_L_J * model_1.delta ** 2)[(1, 1)]
+        self.assertAlmostEqual(1.0, frequency_shift_from_dc_2/frequency_shift_from_Mode, delta=1e-5)
+
 
     def test_set_order(self):
         for order in [0, 1, 2, 1, 0]:
--- tests/test_sense/test_script.py
+++ tests/test_sense/test_script.py
@@ -8,13 +8,9 @@ Phi_0 = 2.07e-15
 
 delta_sym, L_J_sym = sympy.symbols('delta L_J')
 potential_param_symbols = {'L_J': L_J_sym}
-potential_expr = - (delta_sym*Phi_0)**2 / (2*L_J_sym*constants.h)
-
+potential_expr = (delta_sym*Phi_0)**2 / (2*L_J_sym*constants.h)
+delta_min_guess = 0.0
 order = 2
-
-model = Model()
-model.set_order(order)
-model.set_potential(potential_expr, potential_param_symbols)
 
 Z = 50  # ohms
 v_p = 1.1817e8  # m/s
@@ -22,13 +18,29 @@ l = 0.007809801198766881  # m
 L_0 = Z / v_p  # H/m
 C_0 = 1 /(v_p*Z)  # F/m
 x_J = 0.002626535356654626  # m
-L_J = 1e-11
+L_J_1 = 1e-11
+Delta_L_J = 0.0005*L_J_1
+L_J_2 = L_J_1 + Delta_L_J
 
-model.set_resonator_params(x_J=x_J, L_0=L_0, l=l, C_0=C_0)
+model_1 = Model()
+model_1.set_order(order)
+model_1.set_potential(potential_expr, potential_param_symbols)
+model_1.set_resonator_params(x_J=x_J, L_0=L_0, l=l, C_0=C_0)
+potential_params = {'L_J': L_J_1}
+model_1.set_potential_params(potential_params, delta_min_guess=delta_min_guess)
+model_1.set_modes(['a'])
 
-delta_min_guess = None
-potential_params = {'L_J': L_J}
+model_2 = Model()
+model_2.set_order(order)
+model_2.set_potential(potential_expr, potential_param_symbols)
+model_2.set_resonator_params(x_J=x_J, L_0=L_0, l=l, C_0=C_0)
+potential_params = {'L_J': L_J_2}
+model_2.set_potential_params(potential_params, delta_min_guess=delta_min_guess)
+model_2.set_modes(['a'])
 
-model.set_potential_params(potential_params, delta_min_guess=delta_min_guess)
+print(model_2.modes['a'].frequency-model_1.modes['a'].frequency)
 
-print(model.resonator_params['L_J'])
+test = model_1.dc_func(2,'L_J')*Delta_L_J*model_1.delta**2
+print(test[(1,1)])
+
+pass
